66ba8fdead81cf8cedf0258d36f9eb1c0b226752
Petr <pvmezentsev@gmail.com>
2024-04-06 15:30:11 +0700

    Add new method and test for new branch

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayNiceJob(){
+		return "Hello world pochti sovsem ne tormozit, nice job, hunter!";
+	}
 }
